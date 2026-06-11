71954a09baa226c06220f7049748eecf6c78a03f
turaabhaider <turaab.haider0110@gmail.com>
2026-06-11 10:51:22 -0700

    origin

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -114,7 +114,8 @@ const HomePage = ({ onLoginClick }) => {
           <div className="hp-nav-links">
             <a href="#products" className="hp-nav-link">Collections</a>
             <a href="#" className="hp-nav-link">Pricing</a>
-            
+            <a href="#" className="hp-nav-link">About Us</a>
+            <a href="#" className="hp-nav-link">Contact</a>
           </div>
           <div className="hp-nav-actions">
             <button className="hp-nav-btn-outline" onClick={onLoginClick}>Client Login</button>
